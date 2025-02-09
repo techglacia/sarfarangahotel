4bb81136df6364f00932203e629d332149e3239f
Muhammad Yasir <100397581+yasserraahiimi@users.noreply.github.com>
2025-02-09 23:13:25 +0500

    Update Home.js

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -60,23 +60,7 @@ const Home = () => {
       <Location />
       <Feedback />
 
-      <div className="morning2" style={{ display: 'flex', flexDirection: 'row', paddingTop: '0vh' }}>
-        <div className="about" style={{ display: 'flex', flexDirection: "column" }}>
-          <h1 className="welcome" style={{ paddingLeft: '5vw', fontSize: '8vh' }}>ABOUT US</h1>
-          <h4 className="aboutsub ourStory" style={{ paddingLeft: '5vw', fontSize: '5vh', fontFamily: 'Lato, sans-serif', fontWeight: '500' }}>Our Story</h4>
-          <p className="welcomep" style={{ paddingLeft: '5vw', paddingRight: '5vw', fontFamily: 'Lato, sans-serif', fontWeight: '500', fontSize: '3.5vh', color: 'GrayText' }}>
-            The hotel is surrounded by a range of picturesque landscapes, providing guests with breathtaking views of the surrounding areas. The front of the hotel is enhanced by a well-maintained garden, while the rooms offer a serene atmosphere perfect for relaxation. Whether you are here for business or leisure, Hotel Sarfaranga promises a comfortable and memorable stay.
-          </p>
-          <div className="nightimgmain" style={{ display: 'flex', flexDirection: 'column' }}>
-            <img loading="lazy" className="nightimg" src="hotelAssets/chair1.jpeg" alt="" height="25vh" style={{ width: '90vw' }} />
-            <img loading="lazy" className="nightimg" src="hotelAssets/chair2.jpeg" alt="" height="25vh" style={{ width: '90vw' }} />
-          </div>
-          <h4 className="aboutsub" style={{ paddingLeft: '5vw', fontSize: '5vh', fontFamily: 'Lato, sans-serif', fontWeight: '500' }}>Where We Are Located</h4>
-          <p className="welcomep" style={{ paddingLeft: '5vw', paddingRight: '5vw', fontFamily: 'Lato, sans-serif', fontWeight: '500', fontSize: '3.5vh', color: 'GrayText' }}>
-            Hotel Sarfaranga Skardu offers a perfect blend of tranquility and comfort, allowing guests to experience the serene beauty of Skardu. Located on Airport Road, Skardu, the hotel is only 5-7 minutes from Skardu Airport. Surrounded by stunning landscapes, guests can easily explore nearby attractions like Blind Lake, Shigar, Sarfanga, and Kachura Lake, all within a 15 to 40-minute drive.
-          </p>
-        </div>
-      </div>
+    
 
       <Footer />
     </div>
