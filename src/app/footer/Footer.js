90eb94e8bb6dd26c533a8a0108602cd7ca132805
Muhammad Yasir <100397581+yasserraahiimi@users.noreply.github.com>
2025-02-09 22:48:47 +0500

    Update Footer.js

--- src/app/footer/Footer.js
+++ src/app/footer/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Footer() {
   return (
     <>
-      <footer className="bg-gray-900">
+      <footer className="bg-white-900">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
