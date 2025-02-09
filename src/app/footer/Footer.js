029d1f7740d35ec2667d8469316a253edf92b4b4
Muhammad Yasir <100397581+yasserraahiimi@users.noreply.github.com>
2025-02-09 21:47:37 +0500

    Update Footer.js

--- src/app/footer/Footer.js
+++ src/app/footer/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import '../styles/Home.css';
 import Link from 'next/link';
 
 function Footer() {
   return (
     <>
-      <footer className="bg-white bg-gray-900">
+      <footer className="bg-gray-900">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <a href="" className="flex items-center">
                 <img
                   src="hotelAssets/Logo.jpeg"
                   className="h-8 me-3"
@@ -24,20 +23,12 @@ function Footer() {
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact</h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-<<<<<<< HEAD
-                    <Link href="/contact" className="hover:underline">
-=======
                     <Link href="/contact" className="hover:underline text-black">
->>>>>>> 5b83e56 (Updated files after npm rebuild)
                       Email Us
                     </Link>
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <a href="https://tailwindcss.com/" className="hover:underline">
-=======
                     <a href="https://tailwindcss.com/" className="hover:underline text-black">
->>>>>>> 5b83e56 (Updated files after npm rebuild)
                       +92-340 8905227
                     </a>
                   </li>
@@ -47,23 +38,12 @@ function Footer() {
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-                    <a
-                      href="https://www.instagram.com/hotelsarfarangaskardu?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-<<<<<<< HEAD
-                      className="hover:underline"
-=======
-                      className="hover:underline text-black"
->>>>>>> 5b83e56 (Updated files after npm rebuild)
-                    >
+                    <a href="https://www.instagram.com/hotelsarfarangaskardu" className="hover:underline text-black">
                       Instagram
                     </a>
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <a href="https://www.facebook.com/sarfarangahotel/" className="hover:underline">
-=======
                     <a href="https://www.facebook.com/sarfarangahotel/" className="hover:underline text-black">
->>>>>>> 5b83e56 (Updated files after npm rebuild)
                       Facebook
                     </a>
                   </li>
@@ -73,20 +53,12 @@ function Footer() {
                 <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                   <li className="mb-4">
-<<<<<<< HEAD
-                    <a href="#" className="hover:underline">
-=======
                     <a href="#" className="hover:underline text-black">
->>>>>>> 5b83e56 (Updated files after npm rebuild)
                       Privacy Policy
                     </a>
                   </li>
                   <li>
-<<<<<<< HEAD
-                    <Link href="/terms" className="hover:underline">
-=======
                     <Link href="/terms" className="hover:underline text-black">
->>>>>>> 5b83e56 (Updated files after npm rebuild)
                       Terms &amp; Conditions
                     </Link>
                   </li>
@@ -99,38 +71,6 @@ function Footer() {
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               @ 2025 <a className="hover:underline">©Hotel Sarfanga Skardu™</a>. Powered By Tech Glacia
             </span>
-            <div className="flex mt-4 sm:justify-center sm:mt-0">
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 8 19"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Facebook page</span>
-              </a>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 21 16"
-                >
-                  <path
-                    d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"
-                  />
-                </svg>
-                <span className="sr-only">Discord community</span>
-              </a>
-            </div>
           </div>
         </div>
       </footer>
